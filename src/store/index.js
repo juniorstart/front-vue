d12aa96bdcb39a70dc3d43c18258854fd5e2f86b
nanoDW <s.fluder@interia.eu>
2020-07-13 08:12:49 +0200

    Add first unit test, fix problems with config composition api, jest and vuetify

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    isJuniorStart: false,
   },
   mutations: {
+    CHANGE_JUNIORSTART(state) {
+      state.isJuniorStart = !state.isJuniorStart;
+    },
   },
   actions: {
+    changeJuniorStart({ commit }) {
+      commit('CHANGE_JUNIORSTART')
+    }
   },
   modules: {
   }
